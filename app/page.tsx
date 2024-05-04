52efb245e706dbb38c51ab3286f3c121ad7ba63d
Bruno Castro <miroldols@gmail.com>
2024-05-04 15:18:53 -0300

    feat: add initial header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import Header from "./_components/header";
+
 export default function Home() {
-  return (
-    <div className="items-center bg-red-500  px-5 py-10">
-      <h1>Home</h1>
-    </div>
-  );
+  return <Header />;
 }
